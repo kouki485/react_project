31187ee3434efee405151ec15c970ad03b232a91
kouki485 <mwd3145@gmail.com>
2023-07-15 22:24:00 +0900

    fin

--- line-clone/src/components/Line.js
+++ line-clone/src/components/Line.js
@@ -1,13 +1,33 @@
-import React,{ useState } from 'react'
+import React,{ useEffect, useState } from 'react'
 import SignOut from './SignOut'
-import { db } from '../firebase'
+import { auth, db } from '../firebase'
+import SendMessage from './SendMessage';
 
 
 function Line() {
-    const [messages,setMessages] = useState();
+    const [messages,setMessages] = useState([]);
+    useEffect(() => {
+      db.collection("messages")
+      .orderBy("createdAt")
+      .limit(50)
+      .onSnapshot((snapshot) => {
+        setMessages(snapshot.docs.map((doc) => doc.data()));
+      }) 
+    },[])
   return (
     <div>
         <SignOut />
+        <div className='msgs'>
+          {messages.map(({id,text,photoURL,uid}) => (
+            <div>
+              <div key={id} className={`msg ${uid === auth.currentUser.uid ? "sent" : "received"}`}>
+                <img src={photoURL} alt="" />
+                <p>{text}</p>
+              </div>
+            </div>
+          ))}
+          <SendMessage />
+        </div>
     </div>
   )
 }
